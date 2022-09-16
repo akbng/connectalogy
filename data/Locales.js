a689e90e906013fe838bda322964e4efbfe4041e
Ankan Bag <91788199+akbng@users.noreply.github.com>
2022-09-16 21:13:38 +0530

    add data about available Locales

---
+++ data/Locales.js
@@ -0,0 +1,9 @@
+const Locales = [
+  { id: "en", label: "english", locale: "English" },
+  { id: "es", label: "español", locale: "Español" },
+  { id: "fr", label: "français", locale: "Français" },
+  { id: "hi", label: "hindi", locale: "हिंदी" },
+  { id: "bn", label: "bengali", locale: "বাংলা" },
+];
+
+export default Locales;
