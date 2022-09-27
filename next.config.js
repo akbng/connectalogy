dd5f7fa7466749e7ca77bf5ffa9668e3df3eaa71
Ankan Bag <91788199+akbng@users.noreply.github.com>
2022-09-27 23:30:33 +0530

    add feature policy header in next config

--- next.config.js
+++ next.config.js
@@ -11,6 +11,10 @@ const nextConfig = {
             key: "Cache-Control",
             value: "public, max-age=31536000, immutable",
           },
+          {
+            key: "Feature-Policy",
+            value: "autoplay 'self'",
+          },
         ],
       },
     ];
