e9d10510272ea3063d020dc3a696782b1496c681
Ankan Bag <91788199+akbng@users.noreply.github.com>
2022-09-29 00:12:33 +0530

    add options component for multiple option

---
+++ components/Options.jsx
@@ -0,0 +1,80 @@
+import { useContext, useRef, useState } from "react";
+import PropTypes from "prop-types";
+
+import Option from "./Option";
+import styles from "../styles/Options.module.css";
+import { GameContext } from "../providers/GameProvider";
+
+const Options = ({
+  className,
+  options,
+  rightOption,
+  correctAns,
+  setCorrectAns,
+  lang,
+  optionNumbers,
+}) => {
+  const {
+    state: { isSoundOn, mode },
+    updateState,
+  } = useContext(GameContext);
+  const [wrongOptions, setWrongOptions] = useState([]);
+
+  const errorSound = useRef(
+    typeof Audio !== "undefined"
+      ? new Audio("/sounds/error_click.mp3")
+      : undefined
+  );
+  const successSound = useRef(
+    typeof Audio !== "undefined"
+      ? new Audio("/sounds/success_click.mp3")
+      : undefined
+  );
+
+  const onUserSelect = (opt) => () => {
+    if (opt === rightOption) {
+      if (isSoundOn) successSound.current.play();
+      setCorrectAns(opt);
+      return;
+    }
+    if (isSoundOn) errorSound.current.play();
+    if (mode !== "hard" && !wrongOptions.length)
+      setWrongOptions([...wrongOptions, opt]);
+    else updateState({ gameOver: true });
+  };
+
+  return (
+    <div className={[styles.options, className].join(" ")}>
+      {options.map((opt, i) => (
+        <Option
+          key={i}
+          optionNum={optionNumbers[i]}
+          label={lang[opt]}
+          className={[
+            styles.option,
+            wrongOptions.includes(opt) ? styles.wrong_option : "",
+            correctAns === opt ? styles.correct_option : "",
+          ].join(" ")}
+          color={
+            wrongOptions.includes(opt) || correctAns === opt
+              ? "#fff"
+              : "#115e59"
+          }
+          onClick={onUserSelect(opt)}
+        />
+      ))}
+    </div>
+  );
+};
+
+Options.propTypes = {
+  className: PropTypes.string,
+  options: PropTypes.arrayOf(PropTypes.object).isRequired,
+  rightOption: PropTypes.string.isRequired,
+  correctAns: PropTypes.string.isRequired,
+  setCorrectAns: PropTypes.func.isRequired,
+  lang: PropTypes.object.isRequired,
+  optionNumbers: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Options;
