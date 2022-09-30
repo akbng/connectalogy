8e9407fa78da06fdef48109165a9947d4d4a620c
Ankan Bag <91788199+akbng@users.noreply.github.com>
2022-09-30 22:47:10 +0530

    add pause function to the timer component

--- components/Timer.jsx
+++ components/Timer.jsx
@@ -5,9 +5,11 @@ import PropTypes from "prop-types";
 import Ring from "./Ring.svg.jsx";
 import styles from "../styles/Timer.module.css";
 
-const Timer = ({ time, size, color, className, onComplete }) => {
+const Timer = ({ time, size, color, className, onComplete, pause }) => {
   const [remainingTime, setRemainingTime] = useState(time);
   const [startTime] = useState(new Date());
+  const [pausedTime, setPausedTime] = useState(null);
+  const [offset, setOffset] = useState(0);
   const [complete, setComplete] = useState(false);
   const [ringOffset, setRingOffset] = useState(0);
   const radius = ~~(size / 2) - Math.min(8, size * 0.1);
@@ -19,13 +21,13 @@ const Timer = ({ time, size, color, className, onComplete }) => {
       end: new Date(),
     });
 
-    const diff = remainingTime - (seconds + minutes * 60 + hours * 60 * 60);
+    const diff = time - (seconds + minutes * 60 + hours * 60 * 60) + offset;
     setRemainingTime((prevTime) => {
       if (prevTime <= 0) {
         setComplete(true);
         return 0;
       }
-      setRingOffset(circumference - (diff / remainingTime) * circumference);
+      setRingOffset(circumference - (diff / time) * circumference);
       return diff;
     });
   };
@@ -37,9 +39,20 @@ const Timer = ({ time, size, color, className, onComplete }) => {
       clearInterval(timer);
       onComplete();
     }
-
+    if (pause) {
+      setPausedTime(new Date());
+      clearInterval(timer);
+    }
+    if (!pause && pausedTime) {
+      const { seconds, minutes, hours } = intervalToDuration({
+        start: pausedTime,
+        end: new Date(),
+      });
+      setOffset(offset + seconds + minutes * 60 + hours * 60 * 60);
+      setPausedTime(null);
+    }
     return () => clearInterval(timer);
-  }, [complete]);
+  }, [complete, pause, offset]);
 
   return (
     <div className={className} style={{ position: "relative" }}>
@@ -77,6 +90,7 @@ Timer.propTypes = {
   color: PropTypes.string,
   className: PropTypes.string,
   onComplete: PropTypes.func,
+  pause: PropTypes.bool,
 };
 
 Timer.defaultProps = {
