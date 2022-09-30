ebfacb38dce3923b790e69492c65c30bfef9eda7
Ankan Bag <91788199+akbng@users.noreply.github.com>
2022-09-30 22:50:02 +0530

    update the menu component to show up inside Modal

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,6 +1,6 @@
 import { AnimatePresence, motion } from "framer-motion";
 import { useRouter } from "next/router";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 
 import styles from "../styles/Menu.module.css";
 import Locales from "../data/Locales";
@@ -26,51 +26,40 @@ const Menu = () => {
     return Locales.find((loc) => loc.id === state.locale);
   };
 
-  useEffect(() => {
-    router.prefetch("/play");
-    router.prefetch("/learn");
-    router.prefetch("/score");
-  }, []);
-
   return (
-    <div className={styles.outer_container}>
-      <div className={styles.container}>
-        <AnimatePresence mode="wait" initial={false}>
-          {activeTab.match(reg) ? (
-            <motion.div
-              id="language-selector"
-              key={activeTab}
-              className={styles.tab}
-              initial={{ x: 25, opacity: 0 }}
-              animate={{ x: 0, opacity: 1 }}
-              exit={{ x: 25, opacity: 0 }}
-              transition={{ duration: 0.3, ease: "easeInOut" }}
-            >
-              {Locales.map((locale) => (
-                <Selector
-                  key={locale.id}
-                  label={locale.label}
-                  color={state.locale === locale.id ? "#fde68a" : "#fff"}
-                  onClick={selectLocale(locale.id)}
-                />
-              ))}
-            </motion.div>
-          ) : (
-            <motion.div
-              id="default-menu"
-              key="main"
-              className={styles.tab}
-              initial={{ x: -25, opacity: 0 }}
-              animate={{ x: 0, opacity: 1 }}
-              exit={{ x: -25, opacity: 0 }}
-              transition={{ duration: 0.3, ease: "easeInOut" }}
-            >
-              {[
-                "play",
-                "learn",
-                "score board",
-                `${getActiveLocale().label}`,
-              ].map((item, i) => (
+    <div className={styles.container}>
+      <AnimatePresence mode="wait" initial={false}>
+        {activeTab.match(reg) ? (
+          <motion.div
+            id="language-selector"
+            key={activeTab}
+            className={styles.tab}
+            initial={{ x: 25, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            exit={{ x: 25, opacity: 0 }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+          >
+            {Locales.map((locale) => (
+              <Selector
+                key={locale.id}
+                label={locale.label}
+                color={state.locale === locale.id ? "#fde68a" : "#fff"}
+                onClick={selectLocale(locale.id)}
+              />
+            ))}
+          </motion.div>
+        ) : (
+          <motion.div
+            id="default-menu"
+            key="main"
+            className={styles.tab}
+            initial={{ x: -25, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            exit={{ x: -25, opacity: 0 }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+          >
+            {["play", "learn", "score board", `${getActiveLocale().label}`].map(
+              (item, i) => (
                 <Selector
                   key={i}
                   label={item}
@@ -81,16 +70,16 @@ const Menu = () => {
                       : router.push(`/${item.split(" ")[0]}`)
                   }
                 />
-              ))}
-              <Modes />
-              <div className={styles.controllers}>
-                <Speaker size={35} color="#fff" />
-                <Music size={35} color="#fff" />
-              </div>
-            </motion.div>
-          )}
-        </AnimatePresence>
-      </div>
+              )
+            )}
+            <Modes />
+            <div className={styles.controllers}>
+              <Speaker size={35} color="#fff" />
+              <Music size={35} color="#fff" />
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
