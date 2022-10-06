5e8ee1e6c62e4857d27c600829f31611044f4b41
Ankan Bag <91788199+akbng@users.noreply.github.com>
2022-10-06 15:38:09 +0530

    add api route to create a new user

---
+++ pages/api/v1/user/create.js
@@ -0,0 +1,28 @@
+import connectDB from "../../../../db/connectDB";
+import User from "../../../../models/User";
+
+connectDB();
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    try {
+      const { name, email, image } = req.body;
+      const duplicateUser = await User.exists({ email: email });
+      if (duplicateUser)
+        return res
+          .status(403)
+          .json({ error: true, reason: "User already exists!" });
+      const user = await User.create({ name, email, image });
+      return res.status(200).json({ error: false, data: user });
+    } catch (error) {
+      return res.status(500).json({
+        error: true,
+        reason: error.reason || error.message,
+      });
+    }
+  }
+  res.status(405).json({
+    error: true,
+    reason: `${req.method} is not allowed on this route`,
+  });
+}
