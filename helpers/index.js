01e3b14dcc96590d2beb3b6bd56ef05c589d1745
Ankan Bag <91788199+akbng@users.noreply.github.com>
2022-10-09 02:12:40 +0530

    add api helper function for the frontend

---
+++ helpers/index.js
@@ -0,0 +1,22 @@
+export const getHighScorers = (limit) =>
+  fetch(`/api/v1/highscorers?limit=${limit}`).then((res) => res.json());
+
+export const createNewUser = ({ name, email, image }) =>
+  fetch(`/api/v1/user/create`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({ name, email, image }),
+  }).then((res) => res.json());
+
+export const updateScore = (userId, score) =>
+  fetch(`/api/v1/user/update/score/${userId}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({ score }),
+  }).then((res) => res.json());
