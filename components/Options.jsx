1472268a22a7af66e7c6895b9439a41d83e4f4a5
Ankan Bag <91788199+akbng@users.noreply.github.com>
2022-10-15 23:31:39 +0530

    new state to track the attempts left for each level

--- components/Options.jsx
+++ components/Options.jsx
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 import Option from "./Option";
 import styles from "../styles/Options.module.css";
 import { GameContext } from "../providers/GameProvider";
+import { updateScore } from "../logic/mechanics";
 
 const Options = ({
   className,
@@ -13,6 +14,8 @@ const Options = ({
   setCorrectAns,
   lang,
   optionNumbers,
+  attemptsLeft,
+  setAttemptsLeft,
 }) => {
   const {
     state: { isSoundOn, mode, level, score },
@@ -34,17 +37,19 @@ const Options = ({
 
   const onUserSelect = (opt) => () => {
     if (blockUserAction) return;
+    const { inc, dec } = updateScore(level, mode);
     if (opt === rightOption) {
       if (isSoundOn) successSound.current.play();
       setBlockUserAction(true);
       setCorrectAns(opt);
-      updateState({ score: score + 10 });
+      updateState({ score: score + inc });
       return;
     }
     if (isSoundOn) errorSound.current.play();
-    if (mode !== "hard" && !wrongOptions.length)
-      setWrongOptions([...wrongOptions, opt]);
-    else updateState({ gameOver: true });
+    if (attemptsLeft - 1 === 0) return updateState({ gameOver: true });
+    setAttemptsLeft((currentAttempts) => currentAttempts - 1);
+    setWrongOptions((pops) => [...pops, opt]);
+    updateState({ score: score - dec });
   };
 
   const handleKeystrokes = (e) => {
