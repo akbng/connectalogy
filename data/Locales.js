41f22482b6edb5416848c8220f24b4aa816c53f3
Ankan Bag <91788199+akbng@users.noreply.github.com>
2023-05-14 01:16:00 +0530

    add new translations entry

--- data/Locales.js
+++ data/Locales.js
@@ -1,9 +1,14 @@
 const Locales = [
   { id: "en", label: "english", locale: "English" },
-  { id: "es", label: "español", locale: "Español" },
-  { id: "fr", label: "français", locale: "Français" },
+  { id: "es", label: "spanish", locale: "Español" },
+  { id: "fr", label: "french", locale: "Français" },
   { id: "hi", label: "hindi", locale: "हिंदी" },
   { id: "bn", label: "bengali", locale: "বাংলা" },
+  { id: "ja", label: "japanese", locale: "日本語" },
+  { id: "zh", label: "mandarin", locale: "普通话" },
+  { id: "ko", label: "korean", locale: "한국어" },
+  { id: "ru", label: "russian", locale: "Русский" },
+  { id: "ta", label: "tamil", locale: "தமிழ்" },
 ];
 
 export default Locales;
